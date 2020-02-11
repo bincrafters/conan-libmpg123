8b162f00710a201ba8fa47ff1feb35930d70ee0c
Croydon <git@cr0ydon.com>
2020-02-11 07:35:37 +0100

    Migrate build to CMake
    Patch 0001 adds the new CMake port of revision 4592 which should be part of version 1.25.13+
    
    Patch 0002 is adjusting the CMake files to work with version 1.25.13
    
    The CMake files have the upstream license (LGPL-2.0-only)
    
    This allows to get rid of the Windows registry hacks to patch the use of the correct Windows SDK version, gets rid of msys2 as a build requirement and simplifies the build in general
    
    It also defines mpg123 correctly as C-only

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,7 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
+from conans import ConanFile, tools, CMake
 from conans.tools import Version
 from conans.errors import ConanInvalidConfiguration
 import os
-import glob
-import six
 
 
 class LibMPG123Conan(ConanFile):
@@ -14,57 +12,24 @@ class LibMPG123Conan(ConanFile):
     url = "https://github.com/bincrafters/conan-libmpg123"
     homepage = "https://www.mpg123.de"
     license = "LGPL-2.0-only"
+    exports_sources = ["CMakeLists.txt", "patches/*"]
+    generators = "cmake"
+
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
-
-    # copied from conan, need to make expose it
-    def _system_registry_key(self, key, subkey, query):
-        from six.moves import winreg  # @UnresolvedImport
-        try:
-            hkey = winreg.OpenKey(key, subkey)
-        except (OSError, WindowsError):  # Raised by OpenKey/Ex if the function fails (py3, py2)
-            return None
-        else:
-            try:
-                value, _ = winreg.QueryValueEx(hkey, query)
-                return value
-            except EnvironmentError:
-                return None
-            finally:
-                winreg.CloseKey(hkey)
-
-    def _find_windows_10_sdk(self):
-        """finds valid Windows 10 SDK version which can be passed to vcvarsall.bat (vcvars_command)"""
-        # uses the same method as VCVarsQueryRegistry.bat
-        from six.moves import winreg  # @UnresolvedImport
-        hives = [
-            (winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Wow6432Node'),
-            (winreg.HKEY_CURRENT_USER, r'SOFTWARE\Wow6432Node'),
-            (winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE'),
-            (winreg.HKEY_CURRENT_USER, r'SOFTWARE')
-        ]
-        for key, subkey in hives:
-            subkey = r'%s\Microsoft\Microsoft SDKs\Windows\v10.0' % subkey
-            installation_folder = self._system_registry_key(key, subkey, 'InstallationFolder')
-            if installation_folder:
-                if os.path.isdir(installation_folder):
-                    include_dir = os.path.join(installation_folder, 'include')
-                    for sdk_version in os.listdir(include_dir):
-                        if (os.path.isdir(os.path.join(include_dir, sdk_version))
-                                and sdk_version.startswith('10.')):
-                            windows_h = os.path.join(include_dir, sdk_version, 'um', 'Windows.h')
-                            if os.path.isfile(windows_h):
-                                return sdk_version
-        return None
 
     @property
     def _is_msvc(self):
         return self.settings.compiler == "Visual Studio"
 
     def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
         compiler_version = Version(self.settings.compiler.version.value)
         if self.settings.compiler == "Visual Studio" and compiler_version < "14":
             raise ConanInvalidConfiguration("libmpg123 could not be built by Visual Studio < 14")
@@ -74,9 +39,6 @@ class LibMPG123Conan(ConanFile):
             del self.options.fPIC
 
     def build_requirements(self):
-        if tools.os_info.is_windows:
-            if "CONAN_BASH_PATH" not in os.environ:
-                self.build_requires("msys2/20190524")
         if not tools.which("yasm"):
             self.build_requires("yasm/1.3.0")
 
@@ -85,56 +47,43 @@ class LibMPG123Conan(ConanFile):
         extracted_dir = "mpg123-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake    
+
     def build(self):
-        if self._is_msvc:
-            self._build_msvc()
-        else:
-            self._build_configure()
+        if self.conan_data["patches"][self.version]:
+            for patch in self.conan_data["patches"][self.version]:
+                tools.patch(**patch)
 
-    def _build_msvc(self):
-        with tools.chdir(os.path.join(self._source_subfolder, "ports", "MSVC++", "2015", "win32", "libmpg123")):
-            configuration = str(self.settings.build_type) + "_x86"
-            compiler_version = Version(self.settings.compiler.version.value)
-            if compiler_version > "14":
-                win10sdk = self._find_windows_10_sdk()
-                if not win10sdk:
-                    raise ConanInvalidConfiguration("Windows 10 SDK wasn't found")
-                tools.replace_in_file("libmpg123.vcxproj",
-                                      "<WindowsTargetPlatformVersion>8.1</WindowsTargetPlatformVersion>",
-                                      "<WindowsTargetPlatformVersion>%s</WindowsTargetPlatformVersion>" % win10sdk)
-            if self.options.shared:
-                configuration += "_Dll"
-            msbuild = MSBuild(self)
-            msbuild.build(project_file="libmpg123.vcxproj", build_type=configuration)
-
-    def _build_configure(self):
-        with tools.chdir(self._source_subfolder):
-            args = ["--enable-debug=yes" if self.settings.build_type == "Debug" else "--enable-debug=no"]
-            if self.options.shared:
-                args.extend(["--disable-static", "--enable-shared"])
-            else:
-                args.extend(["--enable-static", "--disable-shared"])
-            if self.settings.os == "Android":
-                args.extend(['--disable-buffer', '--with-cpu=generic_fpu'])
-            if self.settings.os == "Emscripten":
-                args.append("--with-default-audio=dummy")
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build.configure(args=args)
-            env_build.make()
-            env_build.install()
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        if self._is_msvc:
+        cmake = self._configure_cmake()
+        cmake.install()
+        # if self._is_msvc:
+        if self.settings.os == "Windows":
             src_folder = os.path.join(self._source_subfolder, "src", "libmpg123")
             self.copy(pattern="*mpg123.h.in", dst="include", src=src_folder)
             self.copy(pattern="*fmt123.h", dst="include", src=src_folder)
             self.copy(pattern="*config.h", dst="include", src=os.path.join(self._source_subfolder, "ports", "MSVC++"))
             self.copy(pattern="*mpg123.h", dst="include", src=os.path.join(self._source_subfolder, "ports", "MSVC++"))
-            self.copy(pattern="*.dll", dst="bin", keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", keep_path=False)
+            # self.copy(pattern="*.dll", dst="bin", keep_path=False)
+            # self.copy(pattern="*.lib", dst="lib", keep_path=False)
 
+        tools.rmdir(os.path.join(self.package_folder, "share"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+    
     def package_info(self):
-        self.cpp_info.libs = ["libmpg123" if self._is_msvc else "mpg123"]
+        self.cpp_info.libs = ["mpg123"]
+        self.cpp_info.names["cmake_find_package"] = "mpg123"
+        self.cpp_info.names["cmake_find_package_multi"] = "mpg123"
+        self.cpp_info.names["pkg_config"] = "libmpg123"
         if self._is_msvc and self.options.shared:
             self.cpp_info.defines.append("LINK_MPG123_DLL")
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs = ["shlwapi"]
