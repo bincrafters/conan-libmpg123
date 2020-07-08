ff1fbc73dceaf94c96d57fb67ad19c8042df0085
Croydon <git@cr0ydon.com>
2020-07-08 02:34:44 +0200

    Update to 1.26.2

--- conanfile.py
+++ conanfile.py
@@ -54,10 +54,8 @@ class LibMPG123Conan(ConanFile):
         return self._cmake
 
     def build(self):
-        if self.conan_data["patches"][self.version]:
-            for patch in self.conan_data["patches"][self.version]:
-                tools.patch(**patch)
-
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
